1614b2beb2598985b2cf7bbdd0521f100ec953c1
fu.xiao <im.fu.xiao@gmail.com>
2025-09-15 09:26:07 +0800

    fix: c++ build error

--- Sources/Runestone/TextView/Core/TextView.swift
+++ Sources/Runestone/TextView/Core/TextView.swift
@@ -590,7 +590,7 @@ open class TextView: UIScrollView {
         textSearchingHelper.findInteraction
     }
 
-    public let textInputView: TextInputView
+    private let textInputView: TextInputView
     private let editableTextInteraction = UITextInteraction(for: .editable)
     private let nonEditableTextInteraction = UITextInteraction(for: .nonEditable)
     @available(iOS 16.0, *)
